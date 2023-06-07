5649ef5527f3b0cf28a9affd100f431a199b6ab8
omerfrkguc <omerguc2541@gmail.com>
2023-06-07 19:46:36 +0300

    CPU-RAM-UseStatus
    CPU ve RAM kullanım durumunu gösteren program.

---
+++ CpuRamUyg/MetroUi/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace MetroUi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCPU = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabelCPU = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarRAM = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabelRAM = new MetroFramework.Controls.MetroLabel();
+            this.chartBilgisayar = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.performanceCounterCPU = new System.Diagnostics.PerformanceCounter();
+            this.performanceCounterRAM = new System.Diagnostics.PerformanceCounter();
+            this.timerIslem = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartBilgisayar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounterCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounterRAM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(36, 91);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(36, 20);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "CPU";
+            // 
+            // metroProgressBarCPU
+            // 
+            this.metroProgressBarCPU.Location = new System.Drawing.Point(159, 88);
+            this.metroProgressBarCPU.Name = "metroProgressBarCPU";
+            this.metroProgressBarCPU.Size = new System.Drawing.Size(326, 23);
+            this.metroProgressBarCPU.TabIndex = 1;
+            // 
+            // metroLabelCPU
+            // 
+            this.metroLabelCPU.AutoSize = true;
+            this.metroLabelCPU.Location = new System.Drawing.Point(548, 90);
+            this.metroLabelCPU.Name = "metroLabelCPU";
+            this.metroLabelCPU.Size = new System.Drawing.Size(40, 20);
+            this.metroLabelCPU.TabIndex = 2;
+            this.metroLabelCPU.Text = "0.0%";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(36, 157);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(38, 20);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "RAM";
+            // 
+            // metroProgressBarRAM
+            // 
+            this.metroProgressBarRAM.Location = new System.Drawing.Point(159, 170);
+            this.metroProgressBarRAM.Name = "metroProgressBarRAM";
+            this.metroProgressBarRAM.Size = new System.Drawing.Size(326, 23);
+            this.metroProgressBarRAM.TabIndex = 4;
+            // 
+            // metroLabelRAM
+            // 
+            this.metroLabelRAM.AutoSize = true;
+            this.metroLabelRAM.Location = new System.Drawing.Point(548, 185);
+            this.metroLabelRAM.Name = "metroLabelRAM";
+            this.metroLabelRAM.Size = new System.Drawing.Size(40, 20);
+            this.metroLabelRAM.TabIndex = 5;
+            this.metroLabelRAM.Text = "0.0%";
+            // 
+            // chartBilgisayar
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartBilgisayar.ChartAreas.Add(chartArea1);
+            legend1.Alignment = System.Drawing.StringAlignment.Center;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Name = "Legend1";
+            this.chartBilgisayar.Legends.Add(legend1);
+            this.chartBilgisayar.Location = new System.Drawing.Point(36, 208);
+            this.chartBilgisayar.Name = "chartBilgisayar";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "CPU";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "RAM";
+            this.chartBilgisayar.Series.Add(series1);
+            this.chartBilgisayar.Series.Add(series2);
+            this.chartBilgisayar.Size = new System.Drawing.Size(552, 240);
+            this.chartBilgisayar.TabIndex = 6;
+            this.chartBilgisayar.Text = "chart1";
+            // 
+            // performanceCounterCPU
+            // 
+            this.performanceCounterCPU.CategoryName = "Processor";
+            this.performanceCounterCPU.CounterName = "% Processor Time";
+            this.performanceCounterCPU.InstanceName = "_Total";
+            // 
+            // performanceCounterRAM
+            // 
+            this.performanceCounterRAM.CategoryName = "Memory";
+            this.performanceCounterRAM.CounterName = "% Committed Bytes In Use";
+            // 
+            // timerIslem
+            // 
+            this.timerIslem.Interval = 1000;
+            this.timerIslem.Tick += new System.EventHandler(this.timerIslem_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 492);
+            this.Controls.Add(this.chartBilgisayar);
+            this.Controls.Add(this.metroLabelRAM);
+            this.Controls.Add(this.metroProgressBarRAM);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabelCPU);
+            this.Controls.Add(this.metroProgressBarCPU);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Form1";
+            this.Text = "CPU && RAM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartBilgisayar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounterCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounterRAM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCPU;
+        private MetroFramework.Controls.MetroLabel metroLabelCPU;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarRAM;
+        private MetroFramework.Controls.MetroLabel metroLabelRAM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBilgisayar;
+        private System.Diagnostics.PerformanceCounter performanceCounterCPU;
+        private System.Diagnostics.PerformanceCounter performanceCounterRAM;
+        private System.Windows.Forms.Timer timerIslem;
+    }
+}
+
---
+++ CpuRamUyg/MetroUi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetroUi
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerIslem.Start(); 
+        }
+
+        private void timerIslem_Tick(object sender, EventArgs e)
+        {
+            float fCPU = performanceCounterCPU.NextValue(); 
+            float fRAM = performanceCounterRAM.NextValue();
+
+            metroProgressBarCPU.Value = (int)fCPU;
+            metroProgressBarRAM.Value = (int)fRAM;
+
+            metroLabelCPU.Text = string.Format("{0:0.00}%",fCPU);
+            metroLabelRAM.Text = string.Format("{0:0.00}%", fRAM);
+
+            chartBilgisayar.Series["CPU"].Points.AddY(fCPU);
+            chartBilgisayar.Series["RAM"].Points.AddY(fRAM);
+        }
+    }
+}
